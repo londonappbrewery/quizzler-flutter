84ae6bd720b28de13e0b7134041c372fc41183a6
Angela <8798027+angelabauer@users.noreply.github.com>
2019-03-12 16:47:52 +0000

    Add starting questions

--- lib/main.dart
+++ lib/main.dart
@@ -89,3 +89,26 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
