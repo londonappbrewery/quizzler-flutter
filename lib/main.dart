96ee515f4442d885f11b9e8bcd53388673a49bfe
Angela <8798027+angelabauer@users.noreply.github.com>
2019-04-22 10:01:58 +0100

    Update to AndroidX

--- lib/main.dart
+++ lib/main.dart
@@ -90,23 +90,6 @@ class _QuizPageState extends State<QuizPage> {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
