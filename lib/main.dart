18e747474eda5cb991945b8a56cc8735abbe2281
emckinney <elderied@gmail.com>
2019-10-02 12:03:54 -0500

    created question class and added some icons,

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 //new quiz app by Elderied
 
@@ -22,11 +23,45 @@ class Quizzler extends StatelessWidget {
 }
 
 class QuizPage extends StatefulWidget {
+  
+
+
   @override
   _QuizPageState createState() => _QuizPageState();
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+/*
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+
+  ];
+
+    List<bool> answers = [
+    false,
+  true,
+  true,
+  ];
+
+*/
+
+  List <Question> questionBank=[
+
+    Question(q:'You can lead a cow down stairs but not up stairs.', a: false ),
+    Question(q:'Approximately one quarter of human bones are in the feet.', a: true ),
+    Question(q:'A slug\'s blood is green.', a: true ),
+
+
+
+  ];
+
+  int questionNum = 0;
+
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +74,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionBank[questionNum].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,6 +99,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = questionBank[questionNum].questionAnswers;
+                if (correctAnswer == true){
+
+                } else {
+                  print('wrong');
+                }
+                setState(() {
+                  questionNum ++;
+
+                });
               },
             ),
           ),
@@ -82,19 +127,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                questionNum ++;
               },
             ),
           ),
         ),
         Row(
-          children: <Widget>[
-            Icon(
-              Icons.check,
-              color: Colors.green,
-            ),
-          ],
+          children: scoreKeeper
         )
-        //TODO create icons widgets
+        //TODO: create icons widgets
       ],
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class Question {
+
+
+  String questionText;
+  bool questionAnswers;
+
+
+  Question({String q, bool a}){
+
+
+    questionText = q;
+    questionAnswers = a;
+
+  }
+
+}
