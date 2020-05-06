d73530ac662fc7947234e3a0c278ed396f0a9e95
Eduardo Rosas <lalorosas@outlook.com>
2020-05-05 22:17:11 -0500

    Create and use new Question class

---
+++ lib/Question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String question;
+  bool answer;
+
+  Question({String q, bool a}) {
+    question = q;
+    answer = a;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,30 +28,64 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   int currentQuestion = 0;
   List<Icon> scores = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
   void selectedTrue() {
-    if (currentQuestion < questions.length - 1) {
+    if (questions[currentQuestion].answer) {
+      // selected true and answer was true
       setState(() {
-        currentQuestion += 1;
+        scores.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      });
+    } else {
+      // selected true but answer was false
+      setState(() {
+        scores.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
       });
     }
 
-    //TODO: true functionality
+    if (currentQuestion < questions.length - 1) {
+      setState(() {
+        currentQuestion++;
+      });
+    }
   }
 
   void selectedFalse() {
-    if (currentQuestion < questions.length - 1) {
+    if (questions[currentQuestion].answer) {
+      // selected false but answer was true
       setState(() {
-        currentQuestion += 1;
+        scores.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      });
+    } else {
+      // selected false and answer was false
+      setState(() {
+        scores.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
       });
     }
 
-    //TODO: false functionality
+    if (currentQuestion < questions.length - 1) {
+      setState(() {
+        currentQuestion++;
+      });
+    }
   }
 
   @override
@@ -65,7 +100,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[currentQuestion],
+                questions[currentQuestion].question,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
