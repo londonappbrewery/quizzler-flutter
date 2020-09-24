4b59913b6e9567c0d291ac4cbb16fc988fbbdc63
heathkev <kevintuazon45@gmail.com>
2020-09-24 12:28:29 +0800

    Updated list and question change

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,21 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(Icons.close, color: Colors.red)
+  List<Icon> scoreKeeper = [];
+
+  List<bool> answers = [
+    false,
+    true,
+    true
   ];
 
-  List<String> myStrings = ['a', 'b', 'c'];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[currentIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,10 +78,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = answers[currentIndex];
+
+                if(correctAnswer == true){
+                  print('user got it right');
+                }else{
+                  print('user got it wrong');
+                }
+
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(Icons.check, color: Colors.green),
-                  );
+                  currentIndex++;
                 });
               },
             ),
@@ -95,6 +107,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = answers[currentIndex];
+                if(correctAnswer == false){
+                  print('user got it right');
+                }else{
+                  print('user got it wrong');
+                }
+
+                setState(() {
+                  currentIndex++;
+                });
               },
             ),
           ),
