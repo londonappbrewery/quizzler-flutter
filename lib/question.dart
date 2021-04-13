af4f17a1608cec68753d25c3740a671b88ec869c
ajini-sahaejana <ajisahejana@gmail.com>
2021-04-14 03:40:49 +0530

    Update question.dart

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,5 @@ class Question {
   String questionText;
   bool questionAnswer;
 
-  Question(String q, bool a) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  Question(this.questionText, this.questionAnswer);
 }
