0546a3e9c115821b861ec21e958108dd65b6713b
ajini-sahaejana <ajisahejana@gmail.com>
2021-04-13 19:25:52 +0530

    Deprecated button removed

--- lib/main.dart
+++ lib/main.dart
@@ -69,8 +69,10 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
               child: Text(
                 'False',
                 style: TextStyle(
