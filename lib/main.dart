1f2ad5366b7dceb5289e1855d062cb37b98150a0
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-03-27 23:49:29 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(Quizzler());
@@ -26,6 +28,16 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> myIcons = [];
+  int qno = 0;
+  List<String> questions = [
+    'Python is used to write Flutter Code',
+    'Flutter is developed by Google',
+    'Some ramdom true question',
+    'You Have completed the quiz',
+  ];
+
+  List<bool> answers = [false, true, true];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -38,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[qno],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -65,12 +77,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  myIcons.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
+                  if (qno < questions.length - 1) {
+                    if()
+                    qno++;
+                    myIcons.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                  }
                 });
                 //The user picked true.
               },
@@ -93,12 +109,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  myIcons.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-                  );
+                  if (qno < questions.length - 1) {
+                    qno++;
+                    myIcons.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  }
                 });
                 //The user picked false.
               },
@@ -108,14 +127,7 @@ class _QuizPageState extends State<QuizPage> {
         Row(
           children: myIcons,
         )
-        //TODO: Add a Row here as your score keeper
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
